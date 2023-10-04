3e5ae72cd43f007134a7aa8c7fb203b0801a0b68
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-10-04 16:10:58 -0300

    use of javascript

--- Codo_a_codo_23548/Practica/capitulo_javascript/index.js
+++ Codo_a_codo_23548/Practica/capitulo_javascript/index.js
@@ -1,3 +1,14 @@
-var x = 0;
-let y = 1;
-const z = 2;
+var pass = '123';
+var usuario;
+var clave;
+
+usuario = prompt('Enter username:');
+clave = prompt('Enter password:');
+
+if (clave == pass){
+		alert('Hi ' + usuario + '!, how are you?');
+	}
+	
+	else {
+		alert('Invalid password!');
+	}
