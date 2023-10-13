9770998dca2d814c63db969b76640700116f3e0a
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-10-13 11:40:01 -0300

    js

--- Codo_a_codo_23548/Practica/capitulo_javascript/index.js
+++ Codo_a_codo_23548/Practica/capitulo_javascript/index.js
@@ -1,4 +1,44 @@
-function obtenerNombre(){return prompt("Como te llamas?");}
-function callBack(nombre){alert(`Hola ${nombre}, bienvenido!`);}
+var spanUp = document.getElementById("sizeup");
+var spanDown = document.getElementById("sizedown");
+var body = document.body;
 
-callBack(obtenerNombre());
+function sizeUp(){
+	let newSize, currentSize = document.body.style.fontSize;
+	if (currentSize === "") currentSize = "16px";
+	newSize = parseInt(currentSize)+1;
+	document.body.style.fontSize = newSize + "px";
+}
+
+function sizeDown(){
+	let newSize, currentSize = document.body.style.fontSize;
+	if (currentSize === "") currentSize = "16px";
+	newSize = parseInt(currentSize)-1;
+	document.body.style.fontSize = newSize + "px";
+}
+
+spanUp.addEventListener("click", sizeUp);
+spanDown.addEventListener("click", sizeDown);
+
+function showHelp(help) {
+	document.getElementById('help').innerHTML = help;
+}
+
+function makeHelpCallback(help){
+	return function(){
+		showHelp(help);
+	};
+}
+
+function setupHelp() {
+	var helpText = [
+		{'id': 'email', 'help': 'Dirección de correo electrónico'},
+		{'id': 'name', 'help': 'Nombre completo'},
+		{'id': 'age', 'help': 'Edad (debes tener más de 16 años)'}
+	];
+	for (var i = 0; i < helpText.length; i++) {
+		var item = helpText[i];
+		document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+	}
+}
+
+setupHelp();
