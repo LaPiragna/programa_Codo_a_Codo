da2c5ba2a3019d4448a772baffa808f0f19cddce
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-10-20 10:10:24 -0300

    integrador javascript

--- Codo_a_codo_23548/Practica/capitulo_bootstrap/integrador_javascript/js.js
+++ Codo_a_codo_23548/Practica/capitulo_bootstrap/integrador_javascript/js.js
@@ -1,18 +1,45 @@
-var links = document.querySelectorAll(".navbar-nav .nav-link"); // Selecciona todos los links
-var url = window.location.href; // Obtiene la URL de la página actual
+window.onload = function(){
+	var links = document.querySelectorAll(".navbar-nav .nav-link"); // Selecciona todos los links
+	var url = window.location.href; // Obtiene la URL de la página actual
 
-if (url.endsWith("index.html") || url.endsWith("index.html#")){
-	document.body.style.backgroundColor = "blue";
-}
-else if(url.endsWith("conviertete_en_orador.html")){
-	document.body.style.backgroundColor = "green";
-	console.log(true);
-}
+	if (url.endsWith("index.html") || 
+		url.endsWith("index.html#")){
+		links.forEach(function(link, index){
+			if (index == 0) links[index].style.color = "white";
+			else {
+				links[index].style.color = "rgb(30,30,30)";
+				links[index].addEventListener("mouseover", function(event){
+					event.target.style.color ="rgb(200,200,200)";
+				})
+				links[index].addEventListener("mouseout", function(event){
+					event.target.style.color ="rgb(30,30,30)";
+				})
+			}
+		})
+		
+	}
+	else if(url.includes("conviertete_en_orador.html") ||
+		url.includes("conviertete_en_orador.html#")){
+			links.forEach(function(link, index){
+			if (index == 3) links[index].style.color = "white";
+			else {
+				links[index].style.color = "rgb(30,30,30)";
+				links[index].addEventListener("mouseover", function(event){
+					event.target.style.color ="rgb(200,200,200)";
+				})
+				links[index].addEventListener("mouseout", function(event){
+					event.target.style.color ="rgb(30,30,30)";
+				})
+			}
+			})
+	}
 
-/*
-index.html
--primer link blanco
--resto de links negro
--hover al resto de links
--onclick en el resto de linnks
-*/
+	/*
+	index.html
+	-primer link blanco
+	-resto de links negro
+	-hover al resto de links
+	-onclick en el resto de linnks
+	*/
+
+}
