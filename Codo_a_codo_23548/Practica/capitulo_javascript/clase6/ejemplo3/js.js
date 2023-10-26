084eceace1db82225d61f803652ff986af201980
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-10-26 16:59:14 -0300

    review javascript

--- Codo_a_codo_23548/Practica/capitulo_javascript/clase6/ejemplo3/js.js
+++ Codo_a_codo_23548/Practica/capitulo_javascript/clase6/ejemplo3/js.js
@@ -15,4 +15,35 @@ function cambiarLuz(){
 	}
 }
 
+btn.addEventListener("click", cambiarLuz);// Obtiene el elemento body del documento
+var body = document.body;
+
+// Inicializa una bandera para rastrear el modo actual (0 para luz, 1 para oscuro)
+var flag = 0;
+
+// Obtiene el botón por su ID
+var btn = document.getElementById("modoluz");
+
+// Define una función para cambiar el modo de luz
+function cambiarLuz(){
+	// Si el modo actual es luz (0)
+	if (flag == 0) {
+		// Cambia a modo oscuro
+		flag = 1;
+		// Cambia el color de fondo a oscuro
+		body.style.backgroundColor = "rgb(40,40,40)";
+		// Cambia el estilo del botón a light
+		btn.className = "btn btn-light";
+	}
+	else{
+		// Cambia a modo luz
+		flag = 0;
+		// Cambia el color de fondo a blanco
+		body.style.backgroundColor = "rgb(255,255,255)";
+		// Cambia el estilo del botón a dark
+		btn.className = "btn btn-dark";
+	}
+}
+
+// Agrega un event listener al botón para cambiar el modo de luz cuando se hace clic en él
 btn.addEventListener("click", cambiarLuz);
