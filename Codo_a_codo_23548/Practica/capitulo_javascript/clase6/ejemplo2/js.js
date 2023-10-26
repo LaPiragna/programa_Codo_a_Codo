73a3d019457549fc275594b581d07cad660dd236
LaPiragna <gonzalovarelaalagna@gmail.com>
2023-10-26 16:36:32 -0300

    review javascript

--- Codo_a_codo_23548/Practica/capitulo_javascript/clase6/ejemplo2/js.js
+++ Codo_a_codo_23548/Practica/capitulo_javascript/clase6/ejemplo2/js.js
@@ -1,49 +1,24 @@
-/**
- * Arreglo para almacenar objetos de tipo persona.
- * @type {Object[]}
- */
 var arreglo = [];
 
-/**
- * Agrega un objeto persona al arreglo y actualiza el HTML.
- * @function
- * @name cargar
- * @param {Object} persona - El objeto persona a agregar.
- * @returns {void} 
- */
-function cargar(persona){
-	arreglo.push(persona); //agrega al arreglo
-	document.getElementById("tbody").innerHTML += `
-		<tr>
-			<td>${persona.nombre}</td>
-			<td>${persona.edad}</td>
-		</tr>`;	//agrega al html
+function cargarDatos(){
+	const nombre = document.getElementById("nombre").value;
+	const edad = document.getElementById("edad").value;
+	
+	function persona(n, e){
+		this.nombre = n;
+		this.edad = e;
+	}
+	
+	p = new persona(nombre, edad);
+	arreglo.push(p);
+	
+	document.getElementById("tbody").innerHTML +=  `
+    <tr>
+        <td>${nombre}</td>
+        <td>${edad}</td>
+    </tr>`;
+
 }
 
-/**
- * Crea un objeto persona con los datos capturados y llama a la función cargar.
- * @function
- * @name capturaDatos
- * @inner
- * @param {none} 
- * @returns {void} 
- */
-function capturaDatos() { 
-    /**
-     * Crea un objeto persona.
-     * @constructor
-     * @param {string} nombre - El nombre de la persona.
-     * @param {number} edad - La edad de la persona.
-     */
-    function persona(nombre, edad){
-        this.nombre = nombre;
-        this.edad= edad;
-    }
-    var capNombre = document.getElementById('name').value;
-    var capEdad = document.getElementById('edad').value;
-    p = new persona(capNombre, capEdad);
-    cargar(p);
-}
-
-const BTNCLICK = document.getElementById("boton");
-BTNCLICK.addEventListener("click", capturaDatos);
+const boton = document.getElementById("boton");
+boton.addEventListener("click", cargarDatos);
